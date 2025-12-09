3f3d075854e628f1336454d9dd2e5bd2cba119ed
khurram <khurram.m.ilyas@gmail.com>
2025-12-09 19:05:01 +0100

    member class added

---
+++ src/member.py
@@ -0,0 +1,14 @@
+class Member:
+    def __init__(self, member_id, name):
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        self.borrowed_books.append(book)
+
+    def return_book(self, book):
+        self.borrowed_books.remove(book)
+        
+    def get_borrowed_books(self):
+        return self.borrowed_books
